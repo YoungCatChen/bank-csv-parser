cd55b4efab7d5c2c2debf8aadc0eba77f01d10a3
YoungCat <chxuexuan@gmail.com>
2016-02-29 23:32:21 -0800

    Continued.

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ import datetime
 class Transaction(collections.namedtuple('Transaction',
                                          'date amount description type id_')):
   _global_id = 0
-  def __new__(cls, date, amount, description, type=None):
+  def __new__(cls, date, amount, description, type=None, id_=None):
     cls._global_id += 1
     return super(Transaction, cls).__new__(
-        cls, date, amount, description, type, cls._global_id)
+        cls, date, amount, description, type, id_ or cls._global_id)
